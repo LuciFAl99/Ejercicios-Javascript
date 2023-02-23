919a8b43fe69fd6915e29fbcc82d201ca14079d7
LuciFAl99 <lucila.alochis1999@gmail.com>
2023-02-23 15:19:34 -0300

    ejercicios js

---
+++ main.js
@@ -0,0 +1,106 @@
+// ejercicio 1 y 10
+/*
+let miNombre = prompt("Ingrese su nombre:");
+console.log(miNombre)
+// 2 y 10
+let miApellido = prompt("Ingrese su apellido:");
+console.log(miApellido)
+// 3 y 10
+let miEdad = prompt("Ingrese su edad:");
+console.log(miEdad)
+//4 y 10
+let miMascota = prompt("Ingrese su mascota:");
+console.log(miMascota)
+// 5 y 10
+let edadMascota = prompt("Ingrese la edad de su mascota:")
+console.log(edadMascota) 
+
+// ejercicio 6
+console.log(miNombre, miApellido, miEdad, miMascota) 
+
+// ejercicio 7
+let nombreCompleto = miNombre + " " + miApellido
+console.log(nombreCompleto)
+
+
+// ejercicio 8
+let textoPresentacion = "Hola, mi nombre es " + nombreCompleto + " tengo " + miEdad + " años. Mi mascota se llama "+ miMascota + " y tiene " + edadMascota + " meses"
+console.log(textoPresentacion)
+
+// ejercicio 9
+let sumaEdades = miEdad + edadMascota
+console.log(sumaEdades)
+let restaEdades = miEdad - edadMascota
+console.log(restaEdades)
+let productoEdades = miEdad * edadMascota
+console.log(productoEdades)
+let divisionEdades = miEdad / edadMascota
+console.log(divisionEdades)  */
+
+// ejercicio 11
+const alumno = {
+    nombreAlumno: "José",
+    apellido: "Ferreyra",
+    edad: 36,
+    colorOjos: "Marrón",
+    autoFavorito: "Falcon",
+}
+console.table(alumno)
+console.log(alumno.nombreAlumno)
+console.log(alumno.apellido)
+console.log(alumno.edad)
+console.log(alumno.colorOjos)
+console.log(alumno.autoFavorito)
+
+// ejercicio 12
+const mascota = {
+    nombreMascota: "Suni",
+    tipoMascota: "Gata",
+    edadMascota: 9,
+    color: "Blanco",
+    esDormilona: true,    
+}
+console.table(mascota)
+console.log(mascota.nombreMascota)
+console.log(mascota.tipoMascota)
+console.log(mascota.edadMascota)
+console.log(mascota.color)
+console.log(mascota.esDormilona)
+
+//ejercicio 13
+let frutas = ["manzanas", "bananas", "peras", "duraznos", "sandías"]
+console.log(frutas)
+console.log(frutas[0])
+console.log(frutas[1])
+console.log(frutas[2])
+console.log(frutas[3])
+console.log(frutas[4])
+
+//ejercicio 14
+let numeros = [6, 8, 7, 4, 3]
+console.log(numeros)
+console.log(numeros[0])
+console.log(numeros[1])
+console.log(numeros[2])
+console.log(numeros[3])
+console.log(numeros[4])
+
+//ejercicio 15
+
+let familia = ["mamá", "papá", "hermano", "tío", "abuelo"]
+console.log(familia)
+console.log(familia[0])
+console.log(familia[1])
+console.log(familia[2])
+console.log(familia[3])
+console.log(familia[4])
+
+
+
+
+
+
+
+
+
+
